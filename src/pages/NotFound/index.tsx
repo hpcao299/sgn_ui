e068cd0db1190bacf5a2f06e4f2497f66ce90a86
HPhucJ2007 <yourgitemail@example.com>
2023-07-09 21:47:56 +0700

    not found page ui

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import styles from './NotFound.module.css';
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+import config from '@/config';
+
+const cx = classNames.bind(styles);
 
 const NotFoundPage: React.FC = () => {
-    return <div>NotFoundPage</div>;
+    return (
+        <div className={cx('not-found')}>
+            <div className={cx('not-found-title')}>Trang không được tìm thấy</div>
+            <Link to={config.routes.home}>Quay về trang chủ</Link>
+        </div>
+    );
 };
 
 export default NotFoundPage;
