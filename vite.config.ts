afe2320f3c3a0ea6c9a1edc835b972e6b819e88a
HPhucJ2007 <yourgitemail@example.com>
2023-07-11 13:59:44 +0700

    minimize the file size when building

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,34 @@
 import { defineConfig } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 import react from '@vitejs/plugin-react-swc';
 import * as path from 'path';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), svgr()],
+    plugins: [
+        react(),
+        svgr(),
+        visualizer({
+            template: 'treemap', // or sunburst
+            open: true,
+            gzipSize: true,
+            brotliSize: true,
+            filename: 'analyse.html', // will be saved in project's root
+        }),
+    ],
     resolve: {
         alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                },
+            },
+        },
     },
 });
