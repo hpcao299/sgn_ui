86790bff20e14d361669faf889a93d32f7e411b4
HPhucJ2007 <yourgitemail@example.com>
2023-07-11 15:10:42 +0700

    fix formattedDate function

--- src/utils/formattedDate.ts
+++ src/utils/formattedDate.ts
@@ -1,9 +1,6 @@
 export function formattedDate(timestamp: string): string {
     const date = new Date(timestamp);
 
-    // Plus 7 hours to date to UTC+7
-    date.setTime(date.getTime() + 7 * 60 * 60 * 1000);
-
     const formattedDate = date.toLocaleString('vi-VN');
 
     return formattedDate;
