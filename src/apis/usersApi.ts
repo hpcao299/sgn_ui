a7d8b468b64abc05b7d95dd0270366745a92b137
HPhucJ2007 <yourgitemail@example.com>
2023-08-23 15:25:47 +0700

    global state management and firebase

---
+++ src/apis/usersApi.ts
@@ -0,0 +1,20 @@
+import { RegisterUserDto } from '@/types';
+
+const usersApi = {
+    async registerUser(details: RegisterUserDto) {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/users/register`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(details),
+        });
+
+        return response.json();
+    },
+    // async getCurrentUser() {
+    //     const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/users/current-user`)
+    // }
+};
+
+export default usersApi;
---
+++ src/stores/useAuthStore.ts
@@ -0,0 +1,66 @@
+import usersApi from '@/apis/usersApi';
+import { auth } from '@/config/firebase';
+import { CurrentUser } from '@/types';
+import { create } from 'zustand';
+
+interface State {
+    currentUser: CurrentUser | null;
+}
+
+interface Actions {
+    signUp: (email: string, password: string) => Promise<unknown>;
+    login: (email: string, password: string) => Promise<unknown>;
+    getCurrentUser: () => Promise<CurrentUser | null>;
+    setNumItems: (count: number) => void;
+    signOut: () => Promise<unknown>;
+}
+
+const useAuthStore = create<State & Actions>(set => ({
+    currentUser: null,
+    signUp: async (email: string, password: string) => {
+        try {
+            const userCredential = await auth.createUserWithEmailAndPassword(email, password);
+            const user = userCredential.user;
+            await usersApi.registerUser({ email: user?.email || '' });
+            set({ currentUser: { email: user?.email || '', id: user?.uid || '' } });
+            return Promise.resolve(user);
+        } catch (error) {
+            return Promise.reject(error);
+        }
+    },
+    login: async (email: string, password: string) => {
+        try {
+            const userCredential = await auth.signInWithEmailAndPassword(email, password);
+            const user = userCredential.user;
+            return Promise.resolve(user);
+        } catch (error: any) {
+            return Promise.reject(error);
+        }
+    },
+    signOut: async () => {
+        try {
+            await auth.signOut();
+            set({ currentUser: null });
+            return Promise.resolve();
+        } catch (error) {
+            return Promise.reject(error);
+        }
+    },
+    getCurrentUser: async () => {
+        // try {
+        //     const user = await usersApi.getCurrentUser();
+        //     setCurrentUser(user.data);
+        //     return user.data;
+        // } catch (error) {
+        //     console.error(error);
+        // }
+        return null;
+    },
+    setNumItems: (count: number) => {
+        set(state => ({
+            currentUser: { ...state.currentUser, num_items: count },
+        }));
+    },
+}));
+
+export default useAuthStore;
